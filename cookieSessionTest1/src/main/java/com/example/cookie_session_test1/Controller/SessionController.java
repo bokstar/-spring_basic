5c2ebf6c4d3edaa465b6028908358ea78deb50fd
JaeBokShin <tlswoqhr12@naver.com>
2020-05-22 21:28:39 +0900

    modify session source

--- cookieSessionTest1/src/main/java/com/example/cookie_session_test1/Controller/SessionController.java
+++ cookieSessionTest1/src/main/java/com/example/cookie_session_test1/Controller/SessionController.java
@@ -14,16 +14,38 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class SessionController {
 
-    //cookie를 setting
+
+
+    @GetMapping("/")
+    public String sessionImp(HttpServletRequest request){
+
+        HttpSession session= request.getSession();
+
+        // 세선 등록
+        session.setAttribute("id","tlswoqhr12");
+        session.setAttribute("password","jb771054!@");
+
+        // 세션 조회
+        System.out.println(session.getValue("id"));
+        System.out.println(session.getValue("password"));
+        return "home";
+    }
+
+
     @GetMapping("/cookieWrite")
     public String cookieWrite( HttpServletResponse response){
+
+    //cookie를 등록한다.
         CookieGenerator generator = new CookieGenerator();
         generator.setCookieName("name");
         generator.addCookie(response,"javaking");
+
         generator.setCookieName("blog");
         generator.addCookie(response,"www.naver.com");
+
         return "home";
     }
+
     //cookie를 읽어 온다..
     @GetMapping("/cookieRider")
     public String cookieRider(
